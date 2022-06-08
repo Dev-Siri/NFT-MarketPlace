cb98acc3cc74b389f2f034ec91da9e004c743c2a
Dev-Siri <102754004+Dev-Siri@users.noreply.github.com>
2022-06-08 23:54:07 +0530

    Add files via upload

---
+++ NFT React Expo/components/index.js
@@ -0,0 +1,22 @@
+import HomeHeader from "./HomeHeader";
+import NFTCart from "./NFTCart";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsDesc from './DetailsDesc';
+
+export {
+  HomeHeader,
+  NFTCart,
+  SubInfo,
+  People,
+  EndDate,
+  RectButton,
+  CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDesc,
+};
